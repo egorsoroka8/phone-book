1ea91d18b248adce14c30560a64455cabd5d8b22
Егор <egor@MBP-Egor.modem.local>
2023-06-14 21:52:02 +0300

    set logic to input and button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import MySelect from "./components/UI/select/MySelect";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import MyMessage from "./components/UI/message/MyMessage";
 import './components/styles/App.css'
 import sortCountries from "./utils/sortCountries";
-import useButtonEnable from "./hooks/useValidation";
 
 
 function App() {
@@ -13,6 +12,22 @@ function App() {
 
   const [code, setCode] = useState(countries[0].phoneCode);
   const [phone, setPhone] = useState('');
+  const [isNumberValid, setNumberValid] = useState(false);
+
+  
+  const handlePhoneChange = (e) => {
+    let inputValue = e.target.value;
+    inputValue = inputValue.replace(/\D/g, ''); // Remove non-digit characters
+    inputValue = inputValue.slice(0, 10); // Limit to 10 digits
+    setPhone(inputValue);
+
+    const isValid = inputValue.length >= 3 && inputValue.length <= 10;
+    setNumberValid(isValid);
+  };
+
+  const handleSubmit = () => {
+    console.log(code + phone);
+  };
   
   return (
     <div className="App">
@@ -23,14 +38,15 @@ function App() {
         value={code}
       />
       <MyInput
-        placeholder='Type your phone'
+        placeholder='Type phone'
         type='text'
-        onChange={e => setPhone(e.target.value)}
+        onChange={handlePhoneChange}
         value={phone}
       />
       <MyMessage />
-      <MyButton 
-        onClick={console.log(code + phone)}
+      <MyButton
+        disabled={!isNumberValid}
+        onClick={handleSubmit}
       >Submit
       </MyButton>
     </div>
