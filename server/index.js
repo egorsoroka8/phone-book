4a495eaa4ed41a27dfddcb03dfd1f9cec0366add
Егор <egor@MBP-Egor.modem.local>
2023-07-04 22:13:01 +0300

    ws-start

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api', router); 
+app.use('/api', router);
 
 
 const start = async () => {
